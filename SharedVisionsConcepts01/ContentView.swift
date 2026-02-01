ed2b54dc0683c36b3a90005157fc3b7acca0d586
Joseph Simpson <joe@radicalappdev.com>
2026-02-01 07:56:52 -0500

    Initial Commit

---
+++ SharedVisionsConcepts01/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  SharedVisionsConcepts01
+//
+//  Created by Joseph Simpson on 2/1/26.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+            ToggleImmersiveSpaceButton()
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+        .environment(AppModel())
+}
---
+++ SharedVisionsConcepts01/SharedVisionsConcepts01App.swift
@@ -0,0 +1,33 @@
+//
+//  SharedVisionsConcepts01App.swift
+//  SharedVisionsConcepts01
+//
+//  Created by Joseph Simpson on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct SharedVisionsConcepts01App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.progressive), in: .progressive)
+    }
+}
